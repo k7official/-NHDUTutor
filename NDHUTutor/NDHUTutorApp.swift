2b40b738cf0731218a0f3882ff531354d7fd1a44
k7official <78650685+k7official@users.noreply.github.com>
2023-12-21 23:56:55 +0800

    Initial Commit

---
+++ NDHUTutor/NDHUTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NDHUTutorApp.swift
+//  NDHUTutor
+//
+//  Created by Muhle Musa Khumalo on 2023/12/21.
+//
+
+import SwiftUI
+
+@main
+struct NDHUTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
