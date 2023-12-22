f77365b784a27991941c959a09159b47116f872c
k7official <78650685+k7official@users.noreply.github.com>
2023-12-22 13:41:08 +0800

    added home page

--- NDHUTutor/ContentView.swift
+++ NDHUTutor/ContentView.swift
@@ -8,16 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showingLoginScreen = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text("NDHUTutor")
+                    .fontWeight(.medium)
+                    .font(.system(size: 40))
+                    .foregroundColor(.green)
+                
+                Button("Login"){
+                    showingLoginScreen = true
+                }
+                .foregroundColor(.white)
+                .frame(width: 300, height: 50)
+                .background(Color.green)
+                .cornerRadius(10)
+                
+                Button("Register"){
+                    
+                }
+                .foregroundColor(.white)
+                .frame(width: 300, height: 50)
+                .background(Color.green)
+                .cornerRadius(10)
+                
+                NavigationLink(destination: Text("You are logged in "), isActive: $showingLoginScreen) {
+                    EmptyView()
+                }
+            }
+            .padding()
         }
-        .padding()
+        .navigationBarHidden(true)
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
