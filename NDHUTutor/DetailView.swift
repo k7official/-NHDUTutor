5b75107ed8e1f57b3bce3cfc10bcd3db648e58a3
k7official <78650685+k7official@users.noreply.github.com>
2023-12-29 16:37:33 +0800

    modified detail view

--- NDHUTutor/DetailView.swift
+++ NDHUTutor/DetailView.swift
@@ -13,6 +13,7 @@ struct DetailView: View {
     
     var body: some View {
         VStack {
+            
             ScrollView {
                 VStack(alignment: .leading, spacing: 16) {
                     // Tutor Image
@@ -21,25 +22,25 @@ struct DetailView: View {
                         .scaledToFill()
                         .frame(height: 200)
                         .clipped()
-
+                    
                     // Tutor Name
                     Text(tutor.name)
                         .font(.title)
                         .fontWeight(.bold)
                         .padding(.horizontal)
-
+                    
                     // Hourly Rates
                     Text("Hourly Rates: $\(tutor.hourlyRates)")
                         .foregroundColor(.gray)
                         .padding(.horizontal)
-
+                    
                     // Subjects Taught
                     VStack(alignment: .leading, spacing: 8) {
                         Text("Subjects Taught")
                             .font(.headline)
                             .fontWeight(.bold)
                             .padding(.horizontal)
-
+                        
                         ForEach(tutor.subjectsTaught, id: \.self) { subject in
                             Text(subject)
                                 .foregroundColor(.blue)
@@ -65,11 +66,85 @@ struct DetailView: View {
             }
             .padding(.horizontal)
             .sheet(isPresented: $isBookingSheetPresented) {
-                Text("Booking here")
+                BookingSheetView(tutor: tutor, isBookingSheetPresented: $isBookingSheetPresented)
             }
         }
         // Add more details as needed
+        
+    }
+}
 
+struct BookingSheetView: View {
+    var tutor: Tutor
+    @Binding var isBookingSheetPresented: Bool
+    @State private var selectedSubjectIndex = 0
+    @State private var selectedDate = Date()
+    @State private var selectedTime = Date()
+    @State private var selectedTimeIndex = 0
+    
+    var subjects: [String] {
+        tutor.subjectsTaught
+    }
+    
+    var timeSlots: [String] {
+        ["10:00 AM", "02:00 PM", "04:00 PM", "06:00 PM"]
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+        
+                Text("Make a Booking")
+                    .font(.title)
+                    .bold()
+                    .padding()
+                Form {
+                    
+                    
+                    // Subject Picker
+                    Section {
+                        Picker("Select Subject", selection: $selectedSubjectIndex) {
+                            ForEach(0..<subjects.count, id: \.self) {
+                                Text(subjects[$0])
+                            }
+                        }
+                    }
+                    
+                    // Date Picker
+                    DatePicker("Select Date", selection: $selectedDate, in: Date()..., displayedComponents: [.date])
+                    //.padding()
+                        .datePickerStyle(.graphical)
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(0..<timeSlots.count, id: \.self) { index in
+                                Button(action: {
+                                    selectedTimeIndex = index
+                                }) {
+                                    Text(timeSlots[index])
+                                        .padding()
+                                        .foregroundColor(selectedTimeIndex == index ? .white : .blue)
+                                        .background(selectedTimeIndex == index ? Color.blue : Color.clear)
+                                        .cornerRadius(10)
+                                }
+                            }
+                        }
+                    }
+                    Button("Book Tutor") {
+                        // Handle the booking action with the selected subject, date, and time
+                        print("Booking: Subject - \(subjects[selectedSubjectIndex]), Date - \(selectedDate), Time - \(selectedTime)")
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Close") {
+                        isBookingSheetPresented.toggle()
+                        // dismiss the view
+                    }
+                }
+            }
+        }
     }
 }
 
